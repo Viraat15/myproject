6b49a7f129857bddd97703dac53bc4743297621e
viru <mvvb02@gmail.com>
2024-08-22 05:49:26 +0000

    first commit

---
+++ scriptsample.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#description: interactive shell script
+# author
+# data created:
+read -p "Enter the command to list out content:" c1
+read -p "Enter a command to create a dicitionary:" c2
+read -p "Enter a command to list out the storage space:" c3
+read -p "Enter a command to create a file:" c4
+read -p "Enter a command to commits the git:" c5
+echo "submitted answers"
+echo -e "$c1 /n $c2 /n $c3 /n $c4 /n $c5"
